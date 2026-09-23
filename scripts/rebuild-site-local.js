2c63586604239294f777d69788b1ca0e181c45ba
python lda <385334338@qq.com>
2026-09-23 11:02:08 +0800

    隐藏提问模板和星辰助手上线文档

--- scripts/rebuild-site-local.js
+++ scripts/rebuild-site-local.js
@@ -59,6 +59,7 @@ const PINNED_ARTICLE_ORDER = {
 };
 
 const HIDDEN_ARTICLE_IDS = new Set([
+  "a2980b63aff5e5944f3a8147",
   "4c12206ddddf3471b6e3e5c0",
   "ca40a1a5e77bad3ae2d060b4",
   "7afb2255add78d6610d66d44",
